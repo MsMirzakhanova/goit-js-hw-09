b2ab098f99fc2432d567271d406992dd632299c9
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2022-10-27 16:28:05 +0300

    task 1 disabled btn

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,16 +5,24 @@ const body = document.querySelector("body");
 console.log(body);
 let timerId = null;
 
+
+
 startBtn.addEventListener("click", () => {
+startBtn.disabled = true; 
   timerId = setInterval(() => {
       body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
   }, 1000);
-});
+}) ;
 
 
 stopBtn.addEventListener("click", () => {
+startBtn.disabled = false; 
   clearInterval(timerId);
 });
+
+
+
+
 
 
 
