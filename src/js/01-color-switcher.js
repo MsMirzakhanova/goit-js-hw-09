6b8f5c256a6c5b84fd9e308c8472cc5bcb89f5ea
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2022-10-27 10:16:18 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
+
+console.log(body);
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+      body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+  }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+});
+
+
+
+
 
